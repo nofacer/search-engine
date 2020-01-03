4cfc9e5a985ef7e6b18ca4729f3543c1c5b80969
Xubin Ni <xubin.ni@thoughtworks.com>
2020-01-03 20:52:34 +0800

    Add indexSinglePage in CrawlerService

--- src/main/java/com/xubin/service/CrawlerService.java
+++ src/main/java/com/xubin/service/CrawlerService.java
@@ -3,6 +3,7 @@ package com.xubin.service;
 import com.xubin.repository.LinkRepository;
 import com.xubin.repository.UrlRepository;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -21,9 +22,14 @@ public class CrawlerService {
   private final UrlService urlService;
   private final WordLocationService wordLocationService;
   private final PageService pageService;
+  private final WordService wordService;
 
   private List<String> pages;
   private int depth;
+
+  private List<String> ignoreWords = new ArrayList<>(
+      Arrays.asList("the", "of", "to", "and", "a", "in", "is", "it")
+  );
 
 
   private boolean isIndexed(String url) {
@@ -42,20 +48,37 @@ public class CrawlerService {
         if (document == null) {
           continue;
         }
-        addToIndex(page, document);
+        indexSinglePage(page, document);
       }
 
     }
   }
 
-  private void addToIndex(String url, Document document) {
+  private void indexSinglePage(String url, Document document) {
     if (isIndexed(url)) {
       return;
     }
-
     System.out.println("Indexing" + url);
-    String pageText = document.text();
+    List<String> words = getPageWords(document);
+    bindWordsToUrl(url, words);
+  }
 
+  private void bindWordsToUrl(String url, List<String> words) {
+    long urlId = urlService.getUrlId(url);
+
+    for (int i = 0; i < words.size(); i++) {
+      String currentWord = words.get(i);
+      if (ignoreWords.contains(currentWord)) {
+        continue;
+      }
+      long wordId = wordService.getWordId(currentWord);
+      wordLocationService.save(urlId, wordId, i);
+    }
+  }
+
+  private List<String> getPageWords(Document document) {
+    String pageText = document.text();
+    return pageService.separateWords(pageText);
   }
 
 }
