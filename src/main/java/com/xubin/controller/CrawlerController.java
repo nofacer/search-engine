50b18f197c2ac8f7b7219cf92e6ad05a8b85fe6b
Xubin Ni <xubin.ni@thoughtworks.com>
2020-01-05 20:58:33 +0800

    Change CrawlerController

--- src/main/java/com/xubin/controller/CrawlerController.java
+++ src/main/java/com/xubin/controller/CrawlerController.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,13 @@ public class CrawlerController {
 
   private final CrawlerService crawlerService;
 
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public void hello() throws URISyntaxException {
-    List<String> pages = new ArrayList<>(Arrays.asList("https://www.thoughtworks.com/"));
-    crawlerService.crawl(pages, 1);
+  @RequestMapping(value = "/crawl", method = RequestMethod.POST)
+  public String hello(@RequestHeader(value = "start_page") String startPage,
+      @RequestHeader(value = "depth") int depth)
+      throws URISyntaxException {
+    List<String> pages = new ArrayList<>(Arrays.asList(startPage));
+    crawlerService.crawl(pages, depth);
+    return "Finished";
   }
 
 }
