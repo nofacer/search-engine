f85a408c19dd6eb0780d88e56d1a7168d3611334
Xubin Ni <xubin.ni@thoughtworks.com>
2020-01-07 22:36:19 +0800

    Create Config.java to store constants

---
+++ src/main/java/com/xubin/utils/Config.java
@@ -0,0 +1,11 @@
+package com.xubin.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Config {
+
+  public static List<String> ignoreWords = Arrays
+      .asList("the", "of", "to", "and", "a", "in", "is", "it");
+
+}
